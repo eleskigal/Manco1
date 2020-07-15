2b06d5cb3f53d53d832acafc78d6a32ee0e5786d
eleskigal <68304442+eleskigal@users.noreply.github.com>
2020-07-15 16:04:47 -0500

    Update manco.py
    Ya te saca las filas que pides aunque hayan tickers sin datos.

--- manco.py
+++ manco.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas_datareader import data as pdr
+import pandas_datareader._utils
 import numpy as np
 import datetime as date
 #Importaremos las acciones que conforman el DJI
@@ -7,7 +8,11 @@ tickers = ["^DJI", "JNJ", "WMT", "KO", "DIS", "RG", "PFE", "HD", "UTX", "VZ", "M
 enddate = date.datetime(2019,11,10)
 startdate = date.datetime(2019,10,10)
 data = pd.DataFrame()
-for tick in tickers:
-    data[tick] =pdr.get_data_yahoo(tick, start=startdate, end=enddate)["Close"] 
-    #con pdr.get importamos los datos de las paginas de internet
+for num, tick in enumerate(tickers):
+    try:
+        data[tick] =pdr.get_data_yahoo(tick, start=startdate, end=enddate)["Close"]
+    except pandas_datareader._utils.RemoteDataError as RDE:
+        tick = tickers[num+1]
+        data[tick] =pdr.get_data_yahoo(tick, start=startdate, end=enddate)["Close"]
+        #con pdr.get importamos los datos de las paginas de internet
 print(data.head())
